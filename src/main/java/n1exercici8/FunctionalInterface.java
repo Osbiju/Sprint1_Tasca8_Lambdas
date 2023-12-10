bf18dc9a3731161d34be4e0601905a373bdd7f8f
Osbiju <okeshik@protonmail.com>
2023-12-10 11:47:38 +0100

    Falta exercici8

--- src/main/java/n1exercici8/FunctionalInterface.java
+++ src/main/java/n1exercici8/FunctionalInterface.java
@@ -1,4 +1,5 @@
 package n1exercici8;
 
 public interface FunctionalInterface {
+    public String reverse(String paraula);
 }
--- src/main/java/n1exercici8/Main.java
+++ src/main/java/n1exercici8/Main.java
@@ -1,4 +1,7 @@
 package n1exercici8;
 
 public class Main {
+    public static void main(String[] args) {
+
+    }
 }
