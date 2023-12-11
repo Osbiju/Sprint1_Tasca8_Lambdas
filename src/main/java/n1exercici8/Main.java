183e2e3abc4e5255abedd049df911926ae813ce9
Oscar <okeshik@protonmail.com>
2023-12-11 12:03:46 +0100

    fet

--- src/main/java/n1exercici8/Main.java
+++ src/main/java/n1exercici8/Main.java
@@ -6,9 +6,16 @@ public class Main {
         //Creem una instancia de la interficie creada amb lambdas,
         // i el cos de la lambda implementara el metode reverse()
        FunctionalInterface reverser = (paraula) -> {
+
+           //convertim la paraula en un array de char
            char[] lletres = paraula.toCharArray();
+
+           //iterem larray de chars
            for (int i = 0; i < lletres.length / 2; i++){
+
+               //creem una variable temporal per asignarli les lletres
                char temporal = lletres[i];
+
                lletres[i] = lletres[lletres.length -1 -i];
                lletres[lletres.length -1 -i] = temporal;
            }
